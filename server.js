d406a329a949f9f453aec37353ffafaeb242afba
RamaKrishnaRaoM <ramakrishna709395@gmail.com>
2024-01-23 15:49:03 +0530

    changes updated

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const db = require('./db');
-require('dotenv').config();
+const db = require("./db");
+require("dotenv").config();
 
-const bodyParser = require('body-parser'); 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Welcome to our Hotel');
-})
+app.get("/", function (req, res) {
+  res.send("Welcome to our Hotel");
+});
 
 // Import the router files
-const personRoutes = require('./routes/personRoutes');
-const menuItemRoutes = require('./routes/menuItemRoutes');
+const personRoutes = require("./routes/personRoutes");
+const menuItemRoutes = require("./routes/menuItemRoutes");
 
 // Use the routers
-app.use('/person', personRoutes);
-app.use('/menu', menuItemRoutes);
-  
-app.listen(PORT, ()=>{
-    console.log('listening on port 3000');
-})
+app.use("/person", personRoutes);
+app.use("/menu", menuItemRoutes);
+
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
